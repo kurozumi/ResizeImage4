18d3555bc9d270b591b36e71e18628e3c3a7fbb7
kurozumi <info@a-zumi.net>
2021-03-11 00:22:24 +0900

    E2Eテスト追加

---
+++ selenium/test.py
@@ -0,0 +1,27 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
+
+class E2ETest(unittest.TestCase):
+
+    def setUp(self):
+        options = Options()
+        options.add_argument('--headless')
+        self.driver = webdriver.Chrome(options=options)
+        self.driver.implicitly_wait(30)
+
+    def tearDown(self):
+        self.driver.close()
+        self.driver.quit()
+
+    def test_1(self):
+        self.driver.get("http://localhost:8000")
+        assert "http://localhost:8000" in self.driver.current_url, self.driver.current_url
+
+    def test_2(self):
+        self.driver.get("http://localhost:8000")
+        assert "EC-CUBE" in self.driver.title, self.driver.title
+
+if __name__ == "__main__":
+    unittest.main()
